288823da348d2b367997dfc4fcccc559fd9db28d
anasmunawar2 <anasmunawar2@gmail.com>
2025-09-03 00:37:12 +0500

    fix: correct notification routes import and improve error handling; add Vercel configuration file

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
-import notificationRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
@@ -27,17 +27,21 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 // error handling middleware
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error("Unhandled error:", err);
-  res.status(500).json({ error: err.message || "Interval server error" });
+  res.status(500).json({ error: err.message || "Internal server error" });
 });
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () =>
-      console.log("Server is up and running on PORT:", ENV.PORT)
-    );
+
+    // listen for local development
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () =>
+        console.log("Server is up and running on PORT:", ENV.PORT)
+      );
+    }
   } catch (error) {
     console.error("Failed to start server:", error.message);
     process.exit(1);
@@ -45,3 +49,6 @@ const startServer = async () => {
 };
 
 startServer();
+
+// export for vercel
+export default app;
