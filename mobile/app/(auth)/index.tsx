e7543486e086032dbdae23b3cb7d809647de6034
anasmunawar2 <anasmunawar2@gmail.com>
2025-09-09 02:10:54 +0500

    feat: enhance authentication UI with Google and Apple sign-in options; add related images

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,9 +1,84 @@
-import { Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Index() {
+  const isLoading = false;
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Auth Page</Text>
+    <View className="flex-1  bg-white">
+      <View className="flex-1 px-1 justify-between">
+        <View className="flex-1 justify-center">
+          <View className="items-center">
+            <Image
+              source={require("../../assets/images/auth2.png")}
+              className="size-96"
+              resizeMode="contain"
+            />
+          </View>
+
+          <View className="flex-col gap-2">
+            <TouchableOpacity
+              className="flex-row justify-center items-center bg-white border border-gray-300 rounded-full py-3 px-6"
+              onPress={() => {}}
+              disabled={isLoading}
+              style={{
+                shadowColor: "#000",
+                shadowOffset: { width: 0, height: 1 },
+                shadowOpacity: 0.1,
+                shadowRadius: 2,
+                elevation: 1,
+              }}
+            >
+              {isLoading ? (
+                <ActivityIndicator size="small" color="#000" />
+              ) : (
+                <View className="flex-row justify-center items-center">
+                  <Image
+                    source={require("../../assets/images/google.png")}
+                    className="size-10 mr-3"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-black font-medium text-base">
+                    Continue with Google
+                  </Text>
+                </View>
+              )}
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              className="flex-row justify-center items-center bg-white border border-gray-300 rounded-full py-3 px-6"
+              onPress={() => {}}
+              disabled={isLoading}
+              style={{
+                shadowColor: "#000",
+                shadowOffset: { width: 0, height: 1 },
+                shadowOpacity: 0.1,
+                shadowRadius: 2,
+                elevation: 1,
+              }}
+            >
+              {isLoading ? (
+                <ActivityIndicator size="small" color="#000" />
+              ) : (
+                <View className="flex-row justify-center items-center">
+                  <Image
+                    source={require("../../assets/images/apple.png")}
+                    className="size-8 mr-3"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-black font-medium text-base">
+                    Continue with Apple
+                  </Text>
+                </View>
+              )}
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
