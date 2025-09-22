613a285988ce6a4ce22e4716452b0498ead26978
anasmunawar2 <anasmunawar2@gmail.com>
2025-09-22 20:18:46 +0500

    feat: custom hook created

---
+++ mobile/components/PostsList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const PostsList = () => {
+  return (
+    <View>
+      <Text>PostsList</Text>
+    </View>
+  );
+};
+
+export default PostsList;
