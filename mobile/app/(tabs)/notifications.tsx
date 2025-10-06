26cae80d31a0edd5a18df75d75a0d0a6c92d43da
anasmunawar2 <anasmunawar2@gmail.com>
2025-10-06 22:59:53 +0500

    feat: notifications hook added

--- mobile/app/(tabs)/notifications.tsx
+++ mobile/app/(tabs)/notifications.tsx
@@ -1,7 +1,20 @@
+import { useNotifications } from "@/hooks/useNotfications";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const NotificationsScreen = () => {
+  const {
+    notifications,
+    isLoading,
+    error,
+    refetch,
+    isRefetching,
+    deleteNotification,
+  } = useNotifications();
+
+  const insets = useSafeAreaInsets();
+
   return (
     <View>
       <Text>NotificationsScreen</Text>
